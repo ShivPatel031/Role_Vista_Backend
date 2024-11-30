34b3801dd0fdb1f0be7bb9d7985f618aabb41d70
ShivPatel031 <patelshiv3123@gmail.com>
2024-11-30 10:21:05 +0530

    remove data from next test

--- test.js
+++ test.js
@@ -1,10 +1,3 @@
 
-const name1 = "shiv";
-
-const name2="trijant";
-
-const trijant = ()=>{console.log("This is trijant function.")};
-
-const shivfunction = ()=>{console.log("This  is shiv function")};
 
 
