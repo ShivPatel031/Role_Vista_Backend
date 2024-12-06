917f8aa6b7050e9df4198fe06bb48095212b19de
Trijant <trijantchhanga1065@gmail.com>
2024-12-06 10:27:52 +0530

    created likePost and removeLike controllers and routes.

--- Routes/post.Routes.js
+++ Routes/post.Routes.js
@@ -3,6 +3,7 @@ import {auth} from "../Middlewares/auth.js"
 import {canPost, canRemovePost} from "../Middlewares/postMiddlewares.js"
 import {createPost, getPosts, removePost} from "../Controllers/postController.js"
 import { upload } from "../Config/Multer.js"
+import { likePost, removeLike } from "../Controllers/likeControllers.js";
 
 
 const route = Router();
@@ -10,5 +11,7 @@ const route = Router();
 route.post("/createPost",upload.single("image"),auth,canPost,createPost);
 route.post("/removePost",auth,canRemovePost,removePost);
 route.post("/getPosts",auth,getPosts);
+route.post("/likePost",auth,likePost);
+route.post("/removeLike",auth,removeLike);
 
 export default route;
